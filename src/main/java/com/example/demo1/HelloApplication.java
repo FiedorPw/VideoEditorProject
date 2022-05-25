55e36050bc399cb95b17e55736db004c994974ff
Artur Oleksiewicz <oleksiewicz45@gmail.com>
2022-05-25 19:11:01 +0200

    jakis comit

---
+++ src/main/java/com/example/demo1/HelloApplication.java
@@ -0,0 +1,24 @@
+package com.example.demo1;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        System.out.println("java version: "+System.getProperty("java.version"));  System.out.println("javafx.version: " + System.getProperty("javafx.version"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
